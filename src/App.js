b839520fe389f2e9a6e99dc50e60ffee0d6fe867
Josiah Sprague <josiah.sprague@gmail.com>
2018-04-23 13:25:47 -0400

    Get messages from API and add swipe to dismiss

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Timeout } from 'react';
+import ReactDOM from "react-dom";
+import { createResource, createCache } from "simple-cache-provider";
 import CssBaseline from 'material-ui/CssBaseline';
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 import Grid from 'material-ui/Grid';
@@ -7,30 +9,87 @@ import Message from './components/Message';
 
 import deepPurple from 'material-ui/colors/deepPurple';
 
+const cache = createCache();
+
+const getMessages = createResource((placeholder) => {
+  return new Promise(resolve => {
+    fetch('http://message-list.appspot.com/messages').then((res)=>{
+      res.json().then(body=>{
+        resolve(body);
+      });
+    });
+  });
+}, (placeholder) => placeholder);
+
+class MessageList extends React.Component {
+  render() {
+    const data = getMessages(cache, 'Placeholder');
+    const { dismissMessage, filter } = this.props;
+    return (
+      <React.Fragment>
+      {data.messages.filter(msg=>{
+        return !filter.exclude.includes(msg.id);
+      }).map(msg=>{
+        return (
+          <Grid key={msg.id} item xs={12}>
+            <Message msg={msg} dismissMessage={(id)=>dismissMessage(id)} />
+          </Grid>
+        );
+      })}
+      </React.Fragment>
+    );
+  }
+}
+
 const theme = createMuiTheme({
   palette: {
     primary: deepPurple,
   },
 });
 
-function App() {
-  return (
-    <MuiThemeProvider theme={theme}>
-      <CssBaseline />
-      <Menu />
-      <div style={{ padding: 16, paddingTop: 72 }}>
-      <Grid container spacing={8}>
-        { [...Array(10)].map((item, i)=>{
-          return (
-            <Grid key={i} item xs={12}>
-              <Message />
-            </Grid>
-          )
-        }) }
-        </Grid>
-      </div>
-    </MuiThemeProvider>
-  );
+class App extends React.Component {
+  state = {
+    showHello: false,
+    loadingIndicator: false,
+    dismissed: [],
+  };
+  
+  requestData = () => {
+    this.setState({loadingIndicator: true});
+    ReactDOM.unstable_deferredUpdates(() => {
+      this.setState({loadData: true});
+    });
+  };
+  
+  componentDidMount() {
+    this.requestData();
+  };
+  
+  dismissMessage(id){
+    this.setState({dismissed: [...this.state.dismissed, id]})
+  };
+  
+  render() {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <Menu />
+        <div style={{ padding: 16, paddingTop: 72, overflowX: 'hidden' }}>
+          <Grid container spacing={8}>
+            <Timeout ms={1000}>
+              {didTimeout => {
+                return didTimeout ? (
+                  <span>Loading...</span>
+                ) : (
+                  <MessageList filter={{exclude: this.state.dismissed}} dismissMessage={(id)=>this.dismissMessage(id)} />
+                );
+              }}
+            </Timeout>
+          </Grid>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -3,27 +3,65 @@ import { withTheme } from 'material-ui/styles';
 import Card, { CardHeader, CardContent } from 'material-ui/Card';
 import Avatar from 'material-ui/Avatar';
 import Typography from 'material-ui/Typography';
+import { defineSwipe, Draggable, Swipeable } from 'react-touch-draggable';
+
+const threshold = 100;
+const swipe = defineSwipe({ swipeDistance: threshold });
 
 class Message extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { left: 0 };
+  }
+
+  handleDrag({dx}) {
+    this.setState({ left: dx });
+  }
+
+  handleDragEnd() {
+    const { left } = this.state;
+    const { dismissMessage, msg } = this.props;
+    if (left < threshold) {
+      this.setState({ left: 0 });
+    } else {
+      dismissMessage(msg.id);
+    }
+  }
+
   render() {
+    const { left } = this.state;
+    const { author, content, id, updated } = this.props.msg;
     return (
-      <Card>
-        <CardHeader
-          style={{paddingBottom: 0}}
-          avatar={
-            <Avatar aria-label="Jo Sprague" >
-              JS
-            </Avatar>
-          }
-          title="Jo Sprague"
-          subheader="12 minutes ago"
-        />
-        <CardContent style={{padding: 16}}>
-          <Typography component="p">
-            Yo me presento como Mr. Johnny Cash pero yo ni cash tengo.
-          </Typography>
-        </CardContent>
-      </Card>
+      <Swipeable config={swipe}>
+        <Draggable
+          onDragEnd={() => this.handleDragEnd()}
+          onDrag={(obj, deltas)=> this.handleDrag(deltas)}
+          position={{ left: left }}
+          style={{ translateX: 0 }}>
+          {({ dx }) => {
+            const isOverThreshold = dx > threshold;
+            const opacity = isOverThreshold ? 0.5 : 1;
+            dx = dx > 0 ? dx : 0; // Only allow dragging to the right
+            return (
+              <div style={{ transform: `translateX(${left}px)`, opacity }}>
+                <Card>
+                  <CardHeader
+                    style={{ paddingBottom: 0 }}
+                    avatar={<Avatar aria-label="Jo Sprague">JS</Avatar>}
+                    title={author.name}
+                    subheader="12 minutes ago"
+                  />
+                  <CardContent style={{ padding: 16 }}>
+                    <Typography component="p">
+                      Yo me presento como Mr. Johnny Cash pero yo ni cash tengo.
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </div>
+            );
+          }}
+        </Draggable>
+      </Swipeable>
     );
   }
 }
