7964e85f8f4aa20c4a452fa1304d2249cfe5f61b
Josiah Sprague <josiah.sprague@gmail.com>
2018-04-24 17:40:38 -0400

    Add get more messages function

--- src/components/MessageList/index.js
+++ src/components/MessageList/index.js
@@ -4,58 +4,70 @@ import Grid from 'material-ui/Grid';
 import Button from 'material-ui/Button';
 import Message from '../Message';
 
-const getMessages = async function (pageToken) {
+const getMessages = async function(pageToken) {
   const response = await fetch(
-    `http://message-list.appspot.com/messages${pageToken?'?pageToken='+pageToken:''}`,
+    `http://message-list.appspot.com/messages?limit=100${
+      pageToken ? '&pageToken=' + pageToken : ''
+    }`
   );
   return await response.json();
 };
 
 class MessageList extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       count: 0,
       pageToken: null,
       messages: [],
-      dismissed: [],
+      dismissed: []
     };
   }
-  
+
   componentDidMount() {
-    getMessages().then(data=>{
-      this.setState(Object.assign({}, this.state, data));
+    getMessages().then(res => {
+      this.setState(Object.assign({}, this.state, res));
     });
-  };
-  
-  dismissMessage(id){
-    this.setState({dismissed: [...this.state.dismissed, id]})
-  };
-  
+  }
+
+  dismissMessage(id) {
+    this.setState({ dismissed: [...this.state.dismissed, id] });
+  }
+
   getMoreMessages() {
-    const { pageToken } = this.state;
-    getMessages(pageToken).then(data=>{
-      this.setState(Object.assign({}, this.state, data));
+    const { messages, pageToken } = this.state;
+    getMessages(pageToken).then(res => {
+      this.setState({
+        pageToken: res.pageToken,
+        messages: messages.concat(res.messages)
+      });
     });
-  };
+  }
 
   render() {
-    const { dismissed, messages, pageToken } = this.state;
+    const { dismissed, messages } = this.state;
     return (
       <React.Fragment>
-        {messages.filter(msg=>{
-          return !dismissed.includes(msg.id);
-        }).map(msg=>{
-          return (
-            <Grid key={msg.id} item xs={12}>
-              <Message msg={msg} dismissMessage={(id)=>this.dismissMessage(id)} />
-            </Grid>
-          );
-        })}
-        <Button onClick={()=>{
-          console.log('get more messages', pageToken);
-          this.getMoreMessages();
-        }}>Test</Button>
+        {messages
+          .filter(msg => {
+            return !dismissed.includes(msg.id);
+          })
+          .map(msg => {
+            return (
+              <Grid key={msg.id} item xs={12}>
+                <Message
+                  msg={msg}
+                  dismissMessage={id => this.dismissMessage(id)}
+                />
+              </Grid>
+            );
+          })}
+        <Button
+          onClick={() => {
+            this.getMoreMessages();
+          }}>
+          Get More Messages
+        </Button>
       </React.Fragment>
     );
   }
