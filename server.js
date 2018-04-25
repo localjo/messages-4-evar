17a4a373029eb50907003491b8716d0d088b8e82
Josiah Sprague <josiah.sprague@gmail.com>
2018-04-25 00:37:42 -0400

    use express basic auth

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const auth = require('http-auth');
-const basic = auth.basic({
-  realm: 'Simon Area.',
-  file: __dirname + '/.htpasswd'
-});
-const bodyParser = require('body-parser');
+const basicAuth = require('express-basic-auth')
+const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
-app.use(auth.connect(basic));
+app.use(basicAuth({
+  users: { 'admin': 'admin' },
+  challenge: true
+}))
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
